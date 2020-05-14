4ba7ae2a55e05ef12e5c1c7d5290c8026dde14f1
maxeisen <17mje@queensu.ca>
2020-05-14 16:07:21 -0400

    intro paragraph and 404 page updated

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -1,17 +1,31 @@
 <script>
 </script>
 
-<h1 class="section-title">My Name is Max Eisen</h1>
+<h1 class="section-title-intro">My Name is Max Eisen...</h1>
 <div class="intro-paragraph">
-    <p>I am a Computer Science student at <a class="intro-link" href="https://www.queensu.ca/" target="_blank">Queen's University</a> with a fascination for technology.</p>
-    <p>I enjoy programming (especially the problem-solving aspects), web development, beta-testing, computational thinking, skiing, travelling, playing guitar, new tech, and just hanging out. I am also interested in automation, machine learning, UI/UX, and data analysis.</p>
+    <p class="title-extension">and I am a Computer Science student at <a class="intro-link" href="https://www.queensu.ca/" target="_blank">Queen's University</a> with a fascination for technology.</p>
+    <p>I am also a computational thinker who enjoys programming, software and web development, beta testing, automation, and UI/UX design. In my free time, you can find me skiing, hiking, travelling, playing guitar, and trying new tech.</p>
+    <p>Although I spend a fair amount of time in front of a computer, I am also a social person who prefers to work with, and be around others. Whether with a project team, a customer, a supervisor, or friends, I strive to communicate effectively and confidently.</p>
     <p>Feel free to explore my website, click on things for more information, and <a class="intro-link" href="mailto:max.eisen@queensu.ca" target="_blank">email me</a> if you have any questions or comments.</p>
 </div>
 
 <style>
+.section-title-intro {
+    margin-top: 20px;
+    padding-bottom: 0;
+    margin-bottom: 0;
+    text-align: left;
+}
+
+.title-extension {
+    padding-top: 0;
+    margin-top: 0;
+}
+
 .intro-paragraph {
     font-size: 18px;
-    margin-bottom: 50px;
+    margin-bottom: 30px;
+    line-height: 1.5;
 }
 
 .intro-link {
