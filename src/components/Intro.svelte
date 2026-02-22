7f8d0d6d56283569b90b549b733fe022969c2720
Max Eisen <44880676+maxeisen@users.noreply.github.com>
2026-02-21 19:28:03 -0500

    Remove annoying comma

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -93,7 +93,7 @@
 <div class="intro-container">
     <h1 class="section-title-intro">Who is Max?</h1>
     <div class="intro-paragraph">
-        <p class="title-extension">I am a Software Development Engineer, at <a class="intro-link" href="https://wealthsimple.com" rel="noreferrer" target="_blank">Wealthsimple</a>,
+        <p class="title-extension">I am a Software Development Engineer at <a class="intro-link" href="https://wealthsimple.com" rel="noreferrer" target="_blank">Wealthsimple</a>,
         and a Computer Science graduate from <a class="intro-link" href="https://www.queensu.ca/" rel="noreferrer" target="_blank">Queen's University</a>,
         with a creative and curious mind, a passion for adventure, and a fascination for all technology.</p>
 
